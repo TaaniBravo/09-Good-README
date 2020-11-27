f3025e40bdc17015f06d9fddf25bd9a23b634a5f
Taani Maama <taani1maama@live.com>
2020-11-27 12:44:57 -0800

    Started on the generateMarkdown template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,14 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
 `;
 }
 
